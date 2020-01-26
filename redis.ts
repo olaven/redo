2678d080755f18b894592366589deac0738f266d
olaven <olav@sundfoer.com>
2020-01-26 18:51:44 +0100

    can persist key-value

--- redis.ts
+++ redis.ts
@@ -1,28 +1,33 @@
 const { connect, copy } = Deno
 
+//string = +MESSAGE\r\n
 const encode = (message: string) => {
 
+    const testCommand = `set name guro\r\n`;
+
     const encoder = new TextEncoder();
-    const encoded = encoder.encode(message);
+    const encoded = encoder.encode(testCommand);
     return encoded;
 }
 
+
 const decode = (buffer: Uint8Array) => {
 
-    const decoder = new TextDecoder();
+    const decoder = new TextDecoder("utf-8");
     const decoded = decoder.decode(buffer);
     return decoded;
 }
 
 
-const receiver = async (connection) => {
+const receiveStuff = async (connection) => {
 
     const buffer = new Uint8Array(100);
     const content = await connection.read(buffer);
+
     const decoded = decode(buffer);
 
 
-    console.log("receiver: ", content);
+    console.log("receiveStuff: ", content);
 }
 
 const sendCommand = async (connection, command) => {
@@ -39,7 +44,7 @@ const redisServer = async () => {
 
     const connection = await connect({port: 6379, transport: "tcp"});
 
-    receiver(connection);
+    receiveStuff(connection);
     sendCommand(connection, "set name guro");
 
 }
