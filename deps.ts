98446b1784da72d441aca29f426192e30e6e0c7b
olaven <olav@sundfoer.com>
2020-01-29 22:12:59 +0100

    tested test library from std.

--- deps.ts
+++ deps.ts
@@ -1,3 +1,10 @@
+// program
 export { encode } from "https://deno.land/std/strings/encode.ts";
 export { decode } from "https://deno.land/std/strings/decode.ts";
 export { BufReader, ReadLineResult } from "https://deno.land/std/io/bufio.ts"
+// testing
+export {
+  assertEquals,
+  runTests,
+  test
+} from "https://deno.land/std/testing/mod.ts";
---
+++ test.ts
@@ -0,0 +1,12 @@
+import {
+  assertEquals,
+  runTests,
+  test
+} from "./deps.ts";
+
+
+test("another", () => {
+    assertEquals(1, 1);
+})
+
+runTests();
